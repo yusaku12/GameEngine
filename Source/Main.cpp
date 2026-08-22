18037dcfdb648196517c3828834df64604be4309
Yusaku <sagiri0702@gmail.com>
2026-08-22 20:00:57 +0900

    Add files via upload

---
+++ Source/Main.cpp
@@ -0,0 +1,6 @@
+﻿#include "Pch.h"
+
+static constexpr LONG SCREEN_WIDTH = static_cast<LONG>(1280);
+static constexpr LONG SCREEN_HEIGHT = static_cast<LONG>(720);
+static constexpr LPCWSTR TITLE = L"DX12";
+static constexpr LPCWSTR WINDOW_CLASS = L"DX12Class";
